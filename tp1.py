34161a55648b68951c744b593b2fd9b072877945
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-10-24 18:52:03 +0200

    TP01 finished

--- tp1.py
+++ tp1.py
@@ -1,6 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
 #import math
 
+def plotVS(plot_x, plot_f1, plot_f2, title: str, xlabel: str, ylabel: str, f1Label: str, f2Label: str):
+    """Plots two functions against each other.  
+    - plot_x: the x-axis values
+    - plot_f1: the y-values of the first function
+    - plot_f2: the y-values of the second function
+    - title: the title of the plot
+    - xlabel: the label of the x-axis
+    - ylabel: the label of the y-axis
+    - f1Label: the label of the first function
+    - f2Label: the label of the second function
+    """
+    
+    plt.tight_layout()
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.plot(plot_x, plot_f1, '-k', label=f1Label, linewidth=1)
+    plt.plot(plot_x, plot_f2, '-b', label=f2Label, linewidth=1)
+    plt.legend(prop={'size': 5})
+    plt.legend(fontsize=7)
+    #plt.legend()
+    plt.show()
 
 def bt(p, nb=1) -> np.ndarray:
     """Bernoulli Trials, where p(1) := p, p(0) := 1-p"""
@@ -29,31 +52,15 @@ def testProba_simulation():
     print(f"i.e. {wPerc}% wins and {lPerc}% losses")
 
     distance = round(np.fabs(wPerc-p), mag)
-    print(
-        f"Measured probability distribution was only {distance}% away from the required one.")
+    print(f"Measured probability distribution was only {distance}% away from the required one.")
 
 # sums of n independant Bernoulli trials => === binomial distribution with param B(n, p)
 
 
-def binomial(n, p, k):
-    # print("#trials=", n, "p=", p, "#success=", k)
-    #p = np.longfloat(p); k = np.longfloat(k)
-    #tmp = 
-    #print(type(tmp), f"n={n}, k={k}", "tmp =", tmp)
-
-    tmp1 = np.longfloat(np.math.comb(n, k)) * np.longfloat(p ** k)
-    
-    print(type(tmp1), f"p={p}, tmp1 =", tmp1)
-
-    tmp2 = tmp1 * np.longfloat((1-p) ** (n-k))
-
-    print(type(tmp2), "tmp2", tmp2)
-
-    #print(f"C({n}, {k}) * {p}^{k} * ({1-p})^({n-k}) =", tmp2)
-    print("---------------------\n")
-    #print(type(np.longfloat(p) ** np.longfloat(k)), np.longfloat(p) ** np.longfloat(k))
-    #out = np.longfloat(np.math.comb(n, k)) * (np.longfloat(p) ** np.longfloat(k)) * ((1-p)**(n-k))
-    return tmp2 # np.longlong(tmp) * tmp3
+def binomial(p, n, k):
+    tmp = np.longfloat(np.math.comb(n, k)) * np.longfloat(p ** k)
+    out = np.longfloat((1-p) ** (n-k))
+    return tmp * out
 
 
 def expected_value(P, ys):
@@ -62,12 +69,9 @@ def expected_value(P, ys):
     return np.array([P(yi)*yi for yi in ys], dtype=np.longfloat).sum()
 
 
-def countWinsBin(n, p):
-    ys = np.arange(n+1)
-    def binml(yi): return binomial(n, p, yi)
-    exp_value = expected_value(binml, ys)
-    #np.array(([binomial(n, p, yi)*yi for yi in ys]).sum())
-    return exp_value
+def countWinsBin(p, n):
+    def binml(yi): return binomial(p, n, yi)
+    return expected_value(binml, np.arange(n+1))
 
 
 if __name__ == "__main__":
@@ -76,12 +80,25 @@ if __name__ == "__main__":
         # testProba_simulation()
         n = 700
         p = 0.25
+        out = countWinsBin(p, n)
         k = 25
-        out = countWinsBin(n, p)
-        print(
-            f"\n|| Probability of {k} success for {n} trials:", binomial(n, p, k))
-        print(
-            f"\n|| Expected number of wins out of {n} trials, (p = {p}) is: ", out)
+        print(f"\n|| Probability of {k} success for {n} trials:", binomial(p, n, k))
+        print(f"\n|| Computed Expected number of points out of {n} trials, (p = {p}) is: ", out)
+        print(f"\n|| Actual Expected number of points out of {n} trials, (p = {p}) is: ", countWins(p, n)[0])
+        x_plot = range(100, 1001, 100)
+        plot_f1 = [countWinsBin(p, x) for x in x_plot]
+        plot_f2 = [countWins(p, x)[0] for x in x_plot]
+
+        print("f1", plot_f1)
+        print("\nf2", plot_f2)
+
+        plotVS(x_plot, plot_f1, plot_f2,
+               "Expected Value of binomial distribution VS Actual sum of bernouilli trials",
+               "Number of trials (n)", 
+               "Number of success",
+               "Expected Value of B(n, p)",
+               "Sum of n Bernouilli Trials")
+        
     except ValueError as ve:
         print("Caught Exception:\n   ", "ValueError:", ve)
 
